e9258da10121fba552d2365fc8a63c7bd6724208
chamikabm <kasun.chamika@gmail.com>
2023-05-12 16:26:39 +1000

    getCommandValues function and its tests added.

--- src/__tests__/utils/index.test.ts
+++ src/__tests__/utils/index.test.ts
@@ -1,11 +1,37 @@
 import {
-    testFunction,
+    getCommandValues,
 } from '../../utils';
+import {
+    VALID_COMMAND,
+} from '../../constants';
 
 describe('Testing: app/utils', () => {
-    describe('testFunction', () => {
-        test('it should return true if the function executed.', () => {
-            expect(testFunction()).toBeTruthy();
+
+    describe('getCommandValues', () => {
+        test('it should return [ "FSDFSD"] if the command is FSDFSD', () => {
+            const command = 'FSDFSD';
+            const result = getCommandValues(command);
+            expect(result).toEqual([ command ]);
+        });
+
+        test('it should return [ MOVE ] if the command is MOVE', () => {
+            const result = getCommandValues(VALID_COMMAND.MOVE);
+            expect(result).toEqual([ VALID_COMMAND.MOVE ]);
+        });
+
+        test('it should return [ LEFT ] if the command is LEFT', () => {
+            const result = getCommandValues(VALID_COMMAND.LEFT);
+            expect(result).toEqual([ VALID_COMMAND.LEFT ]);
+        });
+
+        test('it should return [ RIGHT ] if the command is RIGHT', () => {
+            const result = getCommandValues(VALID_COMMAND.RIGHT);
+            expect(result).toEqual([ VALID_COMMAND.RIGHT ]);
+        });
+
+        test('it should return [ REPORT ] if the command is REPORT', () => {
+            const result = getCommandValues(VALID_COMMAND.REPORT);
+            expect(result).toEqual([ VALID_COMMAND.REPORT ]);
         });
     });
 });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,13 @@
-export const testFunction = () => {
-    return true;
+export const getCommandValues = (command: string) => {
+    try {
+        return command.split(/[\s,]+/);
+    } catch (e) {
+        console.error('Error while splitting the commands. Error : ', e);
+        return null;
+    }
+};
+
+export const processCommandUtil = (commandExecuted: string) => {
+    const commandValues = getCommandValues(commandExecuted);
+    console.log(commandValues);
 };
