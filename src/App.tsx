cbb85990d52c356eb8b73cfb07c8e8a586312b39
chamikabm <kasun.chamika@gmail.com>
2023-05-12 16:15:40 +1000

    MUI theme added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {
+  CssBaseline,
+  ThemeProvider,
+} from '@mui/material';
+import theme from './themes';
 import AllRoutes from './routes';
 
 const App = () => {
+
   return (
+    <ThemeProvider
+      theme={theme}
+    >
+      <CssBaseline />
       <AllRoutes />
+    </ThemeProvider>
   );
 };
 
